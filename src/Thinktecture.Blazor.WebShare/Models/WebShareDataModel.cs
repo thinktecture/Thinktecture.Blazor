136418c8c67daeae7f06e8637e6004757a365711
Christian Liebel <christian.liebel@thinktecture.com>
2022-09-23 02:41:44 +0200

    Docs for Web Share API

--- src/Thinktecture.Blazor.WebShare/Models/WebShareDataModel.cs
+++ src/Thinktecture.Blazor.WebShare/Models/WebShareDataModel.cs
@@ -5,15 +5,27 @@ namespace Thinktecture.Blazor.WebShare.Models
 {
     public class WebShareDataModel
     {
+        /// <summary>
+        /// A string representing a title to be shared.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Title { get; set; }
-        
+
+        /// <summary>
+        /// A string representing a text to be shared.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Text { get; set; }
-        
+
+        /// <summary>
+        /// A string representing a URL to be shared.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Url { get; set; }
-        
+
+        /// <summary>
+        /// An array of <see cref="IJSObjectReference" />s pointing to JavaScript File objects to be shared.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public IJSObjectReference[]? Files { get; set; }
     }
--- src/Thinktecture.Blazor.WebShare/WebShareService.cs
+++ src/Thinktecture.Blazor.WebShare/WebShareService.cs
@@ -13,18 +13,39 @@ namespace Thinktecture.Blazor.WebShare
                 "import", "./_content/Thinktecture.Blazor.WebShare/Thinktecture.Blazor.WebShare.js"));
         }
 
+        /// <summary>
+        /// Determines if the Web Share API is supported on the target user agent.
+        /// </summary>
+        /// <returns>A boolean value indicating if the Web Share API is supported.</returns>
         public async Task<bool> IsSupportedAsync()
         {
             var module = await _moduleTask.Value;
             return await module.InvokeAsync<bool>("isSupported");
         }
 
+        /// <summary>
+        /// Determines if the data can be shared via the current user agent.
+        /// </summary>
+        /// <param name="data">The data that is supposed to be shared.</param>
+        /// <returns>A boolean value indicating if the data can be shared.</returns>
+        /// <exception cref="Exception">
+        /// Throws an exception if the canShare() method is not available on the target platform.
+        /// </exception>
         public async ValueTask<bool> CanShareAsync(WebShareDataModel data)
         {
             var module = await _moduleTask.Value;
             return await module.InvokeAsync<bool>("canShare", data);
         }
 
+        /// <summary>
+        /// Shares the given data via the platform-specific sharing mechanism. The task completes if the data was
+        /// successfully shared with another application.
+        /// </summary>
+        /// <param name="data">The data to share.</param>
+        /// <exception cref="Exception">
+        /// Throws an exception if the share() method is not available on the target platform, the data cannot be shared
+        /// or the user dismisses the share operation.
+        /// </exception>
         public async Task ShareAsync(WebShareDataModel data)
         {
             var module = await _moduleTask.Value;

85a552a5d72df658b5f5d4fcfdd9b42a0d0945cd
Christian Liebel <christian.liebel@thinktecture.com>
2022-10-15 09:25:22 +0200

    Add first draft of File Handling API

---
+++ src/Thinktecture.Blazor.FileHandling/FileHandlingServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Thinktecture.Blazor.FileHandling
+{
+    public static class FileHandlingServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFileHandlingService(this IServiceCollection services)
+        {
+            return services.AddScoped<FileHandlingService>();
+        }
+    }
+}

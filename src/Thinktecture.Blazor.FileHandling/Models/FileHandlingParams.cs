e660cf2714d4031187e180a383e80048d1ce5c9c
Christian Liebel <christian.liebel@thinktecture.com>
2022-11-30 15:15:52 +0100

    Add summarys to public API surface

--- src/Thinktecture.Blazor.FileHandling/Models/FileHandlingParams.cs
+++ src/Thinktecture.Blazor.FileHandling/Models/FileHandlingParams.cs
@@ -4,11 +4,17 @@ namespace Thinktecture.Blazor.FileHandling.Models;
 
 public class LaunchParams
 {
-    // The files are actually a read-only list of FileSystemHandles (see
+    // TODO: The files are actually a read-only list of FileSystemHandles (see
     // https://wicg.github.io/web-app-launch/#launchparams-interface), but converting the types currently is hard and
     // most of the time, users will open files only.
+    /// <summary>
+    /// List of <see cref="FileSystemFileHandle"/>s the application was opened with.
+    /// </summary>
     public IReadOnlyList<FileSystemFileHandle> Files { get; }
     
+    /// <summary>
+    /// Represents the URL of launch.
+    /// </summary>
     public string? TargetUrl { get; }
 
     internal LaunchParams(IReadOnlyList<FileSystemFileHandle> files, string? targetUrl)

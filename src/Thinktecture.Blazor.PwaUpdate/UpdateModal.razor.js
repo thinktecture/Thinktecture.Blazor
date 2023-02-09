03ca3d0d45d1a14a39e8d5d49269c898df77ec1e
Patrick Jahr <patrick.jahr@thinktecture.com>
2023-02-09 14:31:47 +0100

    feat(update): update jsinterop

---
+++ src/Thinktecture.Blazor.PwaUpdate/UpdateModal.razor.js
@@ -0,0 +1,20 @@
+﻿const installedStateKey = 'installed';
+const serviceWorkerPropertyKey = 'serviceWorker';
+
+/*
+ * updateFound ok aber für hier nicht ok
+ * services ready => registration => handle event
+ * 
+ **/
+export async function registerUpdateEvent(caller, methodName) {
+    const registration = await navigator.serviceWorker.ready;
+    registration.onupdatefound = () => {
+        const installingServiceWorker = registration.installing;
+        installingServiceWorker.onstatechange = () => {
+            console.log(installingServiceWorker.state);
+            if (installingServiceWorker.state === installedStateKey) {
+                caller.invokeMethodAsync(methodName).then();
+            }
+        }
+    }
+}

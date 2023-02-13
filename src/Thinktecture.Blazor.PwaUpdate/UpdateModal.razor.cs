3da814493828cc58b9aa1f1d44cc5f3f7a0e3394
Patrick Jahr <patrick.jahr@thinktecture.com>
2023-02-13 14:22:24 +0100

    feat(pwa-update): add update service

--- src/Thinktecture.Blazor.PwaUpdate/UpdateModal.razor.cs
+++ src/Thinktecture.Blazor.PwaUpdate/UpdateModal.razor.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
+using Thinktecture.Blazor.PwaUpdate.Services;
 
 namespace Thinktecture.Blazor.PwaUpdate
 {
     public partial class UpdateModal
     {
-        [Inject] private IJSRuntime _jsRuntime { get; set; } = default!;
+        [Inject] private UpdateService _updateService { get; set; } = default!;
 
         [Parameter] public string InformationMessage { get; set; } = string.Empty;
         [Parameter] public RenderFragment? ChildContent { get; set; }
@@ -15,29 +15,14 @@ namespace Thinktecture.Blazor.PwaUpdate
 
         protected override async Task OnInitializedAsync()
         {
-            var module = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Thinktecture.Blazor.PwaUpdate/UpdateModal.razor.js");
-            if (module is not null)
-            {
-                await RegisterForUpdateAvailableNotification(module);
-            }
+            _updateService.UpdateAvailable = () => _newVersionAvailable = true;
+            await _updateService.InitializeServiceWorkerUpdate();
             await base.OnInitializedAsync();
-        }
-
-        private async Task RegisterForUpdateAvailableNotification(IJSObjectReference jSObject)
-        {
-            await jSObject.InvokeVoidAsync("registerUpdateEvent", DotNetObjectReference.Create(this), nameof(OnUpdateAvailable));
         }
 
         private void CollapseModal()
         {
             _collapse = true;
-        }
-
-        [JSInvokable(nameof(OnUpdateAvailable))]
-        public void OnUpdateAvailable()
-        {
-            _newVersionAvailable = true;
-            StateHasChanged();
         }
     }
 }

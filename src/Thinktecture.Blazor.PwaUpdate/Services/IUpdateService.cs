d31b83f1c2a7f9bd2b79878071a60bb3fd02ee46
Patrick Jahr <patrick.jahr@thinktecture.com>
2023-02-17 10:54:29 +0100

    feat(pwa-update): add service interface

---
+++ src/Thinktecture.Blazor.PwaUpdate/Services/IUpdateService.cs
@@ -0,0 +1,11 @@
+﻿namespace Thinktecture.Blazor.PwaUpdate.Services
+{
+    public interface IUpdateService
+    {
+        Action UpdateAvailable { get; set; }
+
+        Task InitializeServiceWorkerUpdateAsync();
+        void OnUpdateAvailable();
+        Task ReloadAsync();
+    }
+}
